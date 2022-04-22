65c2cae9a84c4587c26ef21f9a694e1d1f82d0d0
Alexei Fedotov <alexei.fedotov@gmail.com>
2022-04-22 16:31:53 +0300

    attempt to fix UdfTest

---
+++ scripts/conda/test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -ex
+
+this_dir=$(dirname "${BASH_SOURCE[0]}")
+RECIPE_DIR=${RECIPE_DIR:-${this_dir}}
+
+# Omnisci UDF support uses CLangTool for parsing Load-time UDF C++
+# code to AST. If the C++ code uses C++ std headers, we need to
+# specify the locations of include directories:
+. ${RECIPE_DIR}/get_cxx_include_path.sh
+export CPLUS_INCLUDE_PATH=$(get_cxx_include_path)
+
+export LD_LIBRARY_PATH="$CONDA"/envs/omnisci-dev/pkgs/cuda-toolkit/targets/x86_64-linux/lib/stubs
+
+mkdir -p build
+cd build
+
+make ci_tests
+
